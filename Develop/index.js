9cd5b965e5132acd5452a4f109097a72b9b9f3e8
Sopee Thong <tsopee@gmail.com>
2021-04-09 21:16:13 -0400

    adding write to file function

--- Develop/index.js
+++ Develop/index.js
@@ -52,12 +52,15 @@ const questions = [{
     {
         type: 'input',
         name: 'Questions',
-        message: 'Enter your LinkedIn URL.',
+        message: 'How can questions be submitted about your project',
         
 },];
 
 // TODO: Create a function to write README file
-function writeToFile(generateMarkdown, answers) {}
+function writeToFile(generateMarkdown, answers) {
+    fs.writeFile(generateMarkdown, JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+}
 
 // TODO: Create a function to initialize app
 function init() {}
