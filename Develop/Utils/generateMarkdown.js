a65acab354c83fe0aa6ec702c511686f7de9cff3
Sopee Thong <tsopee@gmail.com>
2021-04-09 22:09:57 -0400

    testing if function

--- Develop/Utils/generateMarkdown.js
+++ Develop/Utils/generateMarkdown.js
@@ -4,7 +4,15 @@ const gnu = "GNU GPL v3 License: GPL v3 (https://img.shields.io/badge/License-GP
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if(data.license === "The MIT License") {
+        data.license = mit;
+    } else if (data.license === "Mozilla Public License 2.0") {
+        data.license = mozilla;
+    } else if (data.license ==="GNU GPL v3") {
+        data.license = gnu;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
