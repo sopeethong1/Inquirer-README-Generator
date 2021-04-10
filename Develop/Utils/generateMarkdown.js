55890b26c4b71d19f1ca5b30ff7c3c9614ef8275
Sopee Thong <tsopee@gmail.com>
2021-04-09 21:22:10 -0400

    adding license const

--- Develop/Utils/generateMarkdown.js
+++ Develop/Utils/generateMarkdown.js
@@ -1,3 +1,7 @@
+const mit = The MIT License
+const mozilla =  Mozilla Public License 2.0
+const gnu = GNU GPL v3
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
