c0d6425b0c696accbfd82261ace2cb81657e4c3b
Sopee Thong <tsopee@gmail.com>
2021-04-09 21:27:27 -0400

    added badging urls

--- Develop/Utils/generateMarkdown.js
+++ Develop/Utils/generateMarkdown.js
@@ -1,6 +1,6 @@
-const mit = The MIT License
-const mozilla =  Mozilla Public License 2.0
-const gnu = GNU GPL v3
+const mit = "The MIT License: MIT(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+const mozilla =  "Mozilla Public License 2.0: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+const gnu = "GNU GPL v3 License: GPL v3 (https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
